9c69dd5ea93775bd860f408b9f8f6a35347df793
SERGEY <sergey@cpan.org>
2001-03-20 00:26:00 +0000

    Import https://cpan.metacpan.org/authors/id/S/SE/SERGEY/Audio-Mixer-0.3.tar.gz

---
+++ Mix.h
@@ -0,0 +1,29 @@
+/*
+ * Library to query / set various sound mixer parameters.
+ *
+ * This code is based on setmixer program by Michal Jaegermann
+ *
+ * Copyright (c) 2000 Sergey Gribov <sergey@sergey.com>
+ * This is free software with ABSOLUTELY NO WARRANTY.
+ * You can redistribute and modify it freely, but please leave
+ * this message attached to this file.
+ *
+ * Subject to terms of GNU General Public License (www.gnu.org) 
+ *
+ * Last update: $Date: 2001/03/19 16:56:47 $ by $Author: sergey $
+ * Revision: $Revision: 1.2 $
+ *
+ */
+
+#define MIXER "/dev/mixer"
+
+int get_param_val(char *cntrl);
+
+int set_param_val(char *cntrl, int lcval, int rcval);
+
+int init_mixer();
+
+int get_params_num();
+
+char *get_params_list();
+
