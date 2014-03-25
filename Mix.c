6390829e5e60019b464d1fe83fb448ea54604170
gregor herrmann <gregoa@debian.org>
2014-03-26 05:44:25 +1300

    Fix building with clang
    Bug: https://rt.cpan.org/Ticket/Display.html?id=94186
    Bug: https://bugs.debian.org/742537

--- Mix.c
+++ Mix.c
@@ -78,7 +78,7 @@ close_mixer() {
 #ifdef DEBUG
   fprintf(stderr, "close_mixer()\n");
 #endif
-  if (mixer_fd < 0) return;
+  if (mixer_fd < 0) return(0);
   close(mixer_fd);
   init_flag = 0;
   mixer_fd = -1;
